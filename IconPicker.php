64698f0da385b7729adf8be04378dfdf977aa1b7
Alexey Shevchenko <ivanovosity@gmail.com>
2017-03-02 04:29:25 +0300

    Init git

---
+++ IconPicker.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace dominus77\iconpicker;
+
+/**
+ * This is just an example.
+ */
+class IconPicker extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
